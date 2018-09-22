c2d626f1af6a210d24745fb0c95c88919fbd6f84
vic3king <vic3coorp@gmail.com>
2018-09-22 21:31:40 +0100

    convert bolleans to truthy and falsy

--- notes-edit.js
+++ notes-edit.js
@@ -39,7 +39,7 @@ window.addEventListener('storage', (e) => {
         notes = JSON.parse(e.newValue)
         note = notes.find((note) => note.id === noteId)
 
-        if (note === undefined) {
+        if (!note) {
             location.assign('../notes/index.html')
         }
 
